b86851820f5cd03dbf16849b769829a49b4082bb
Pedro William <pedro16william@gmail.com>
2025-07-03 11:34:33 -0300

    Foi implementado o teste da controller responsável pelo processo de criação de vagas

--- src/main/java/br/com/pw/gestao/modules/company/dto/JobCreateDTO.java
+++ src/main/java/br/com/pw/gestao/modules/company/dto/JobCreateDTO.java
@@ -1,9 +1,15 @@
 package br.com.pw.gestao.modules.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobCreateDTO {
     @Schema(example="Desenvolvedor Backend Java",requiredMode=Schema.RequiredMode.REQUIRED)
     private String description;
